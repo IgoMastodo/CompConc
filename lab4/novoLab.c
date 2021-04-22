642cf0ef366308cd2783a7d08d4ae4b9dab85459
Igor <42788295+IgorMurras@users.noreply.github.com>
2021-04-22 07:43:34 -0700

    Update novoLab.c

--- lab4/novoLab.c
+++ lab4/novoLab.c
@@ -5,24 +5,16 @@
 #define NTHREADS  4
 
 /* Variaveis globais */
-int x = 0, aux=0;
+int x = 0;
 pthread_mutex_t x_mutex;
 pthread_cond_t x_cond, y_cond;
 
 /* Thread A */
 void *A (void *t) {
-
-  printf("A: Comecei\n");
-  
-
-  printf("Seja bem vindo\n");
-
+  printf("Seja bem vindo!\n");
   pthread_mutex_lock(&x_mutex);
   x++;
-  if (x==1) {
-      printf("A:  x = %d, vai sinalizar a condicao \n", x);
-      pthread_cond_signal(&x_cond);
-  }
+  if (x==1) pthread_cond_broadcast(&x_cond);
   pthread_mutex_unlock(&x_mutex);
 
   pthread_exit(NULL);
@@ -30,50 +22,37 @@ void *A (void *t) {
 
 /* Thread B */
 void *B (void *t) {
-  printf("B: Comecei\n");
-
   pthread_mutex_lock(&x_mutex);
-  aux++;
   if (x < 1) { 
-     printf("B: x= %d, vai se bloquear...\n", x);
      pthread_cond_wait(&x_cond, &x_mutex);
-     printf("B: sinal recebido e mutex realocado, x = %d\n", x);
   }
+  x++;
   printf("Sente-se por favor.\n");
-  if(aux==2){ pthread_cond_signal(&y_cond); }
+  if(x==3) pthread_cond_signal(&y_cond); 
   pthread_mutex_unlock(&x_mutex); 
+  
   pthread_exit(NULL);
 }
 
 /* Thread C */
 void *C (void *t) {
-  printf("C: Comecei\n");
-
   pthread_mutex_lock(&x_mutex);
-  aux++;
-  if (x < 1) { 
-     printf("C: x= %d, vai se bloquear...\n", x);    
-     pthread_cond_wait(&x_cond, &x_mutex);
-     printf("C: sinal recebido e mutex realocado, x = %d\n", x);
-  }
-  printf("Fique a vontade\n");
-  if(aux==2){ pthread_cond_signal(&y_cond); }
+  if (x < 1) pthread_cond_wait(&x_cond, &x_mutex);
+  x++;
+  printf("Fique a vontade.\n");
+  if(x==3) pthread_cond_signal(&y_cond); 
   pthread_mutex_unlock(&x_mutex); 
+  
   pthread_exit(NULL);
 }
 
-// Thread D
+/* Thread D */
 void *D (void *t) {
-  printf("D: Comecei\n");
-
   pthread_mutex_lock(&x_mutex);
-  if (x < 3) { 
-     printf("D: x= %d, vai se bloquear...\n", x);
-     pthread_cond_wait(&y_cond, &x_mutex);
-     printf("D: sinal recebido e mutex realocado, x = %d\n", x);
-  }
+  while(x < 3) pthread_cond_wait(&y_cond, &x_mutex);
   printf("Volte sempre!\n");
   pthread_mutex_unlock(&x_mutex); 
+  
   pthread_exit(NULL);
 }
 
@@ -81,8 +60,6 @@ void *D (void *t) {
 int main(int argc, char *argv[]) {
   int i; 
   pthread_t threads[NTHREADS];
-
-  /* Inicilaiza o mutex (lock de exclusao mutua) e a variavel de condicao */
   pthread_mutex_init(&x_mutex, NULL);
   pthread_cond_init (&x_cond, NULL);
   pthread_cond_init (&y_cond, NULL);
@@ -97,10 +74,10 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < NTHREADS; i++) {
     pthread_join(threads[i], NULL);
   }
-  printf ("\nFIM\n");
 
   /* Desaloca variaveis e termina */
   pthread_mutex_destroy(&x_mutex);
   pthread_cond_destroy(&x_cond);
    pthread_cond_destroy(&y_cond);
 }
+
